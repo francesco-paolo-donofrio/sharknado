05d8e3929c30311735b9a36816e3215a49ea7fdd
Frankosviz <geom.francescopaolo@gmail.com>
2024-06-27 22:11:02 +0200

    Start commit

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,18 @@
 </template>
 
 <script>
-  export default {
-    name: 'App'
+export default {
+  name: 'App',
+  data() {
+    return {
+      boardSize: 20, // dimensioni della griglia di gioco
+      snake: [{ x: 10, y: 10 }], // posizione iniziale del serpente
+      direction: { x: 0, y: 1 }, // direzione iniziale (verso il basso)
+      food: { x: 15, y: 15 }, // posizione iniziale del cibo
+      interval: null,
+    }
   }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
