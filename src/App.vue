09ffeab02e5825aaaceb68e6a880d10afa8603aa
Frankosviz <geom.francescopaolo@gmail.com>
2024-06-27 23:51:34 +0200

    sharknado - end

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,28 @@
 <template>
-  <h1 class="text-center">Ciao</h1>
-  <i class="fa fa-solid fa-home"></i>
+  <div id="app">
+    <div class="d-flex flex-column align-items-center justify-content-center">
+      <h1 class="text-white text-center">Sharknado</h1>
+      <button style="width: 190px; height: 100px" @click="startGame" class="shark-button">
+        <span class="teeth"></span>
+        <span class="teeth"></span>
+        <span class="teeth"></span>
+        <span class="teeth"></span>
+        <span class="teeth"></span>
+        <span class="teeth"></span>
+        <span class="teeth"></span>
+        <span class="teeth"></span>
+        <span class="teeth"></span>
+      </button>
+      <p class="text-white text-center">
+        Click the mouth to let the Shark eat Yotobi!
+      </p>
+      <p class="f-d-eating" id="appearParagraph">
+        Gnam gnam gnam gnam gnam 
+      </p>
+    </div>
+    <div id="game-board" ref="gameBoard"></div>
+
+  </div>
 </template>
 
 <script>
@@ -16,14 +38,18 @@ export default {
     }
   },
   methods: {
+    appearParagraph() {
+      document.getElementById('appearParagraph').style.display = 'block';
+      setTimeout(() => {
+        document.getElementById('appearParagraph').style.display = 'none';
+      }, 2000); // 2000 millisecondi = 2 secondi
+    },
     startGame() {
-      this.interval = setInterval(this.moveSnake, 200);
-      window.addEventListener('keydown', this.changeDirection);
       clearInterval(this.interval); // Pulisci qualsiasi intervallo precedente
       this.snake = [{ x: 10, y: 10 }]; // Reimposta la posizione iniziale del serpente
       this.direction = { x: 0, y: 1 }; // Reimposta la direzione iniziale
       this.food = this.getRandomFoodPosition(); // Genera una nuova posizione del cibo
-      this.interval = setInterval(this.moveSnake, 200);
+      this.interval = setInterval(this.moveSnake, 100);
       window.addEventListener('keydown', this.changeDirection);
     },
     moveSnake() {
@@ -37,7 +63,7 @@ export default {
         alert('Game Over!');
         clearInterval(this.interval);
         return;
-      };
+      }
       // Controlla collisioni con il corpo del serpente
       for (let segment of this.snake) {
         if (head.x === segment.x && head.y === segment.y) {
@@ -45,32 +71,72 @@ export default {
           clearInterval(this.interval);
           return;
         }
-      };
+      }
       this.snake.unshift(head);
 
       // Controlla se il serpente ha mangiato il cibo
       if (head.x === this.food.x && head.y === this.food.y) {
         this.food = this.getRandomFoodPosition(); // Genera una nuova posizione del cibo
+        this.appearParagraph();
       } else {
         this.snake.pop(); // Rimuovi l'ultimo segmento del serpente
       }
 
       this.drawGame();
     },
-  },
+    changeDirection(event) {
+      switch (event.key) {
+        case 'ArrowUp':
+          if (this.direction.y === 0) this.direction = { x: 0, y: -1 };
+          break;
+        case 'ArrowDown':
+          if (this.direction.y === 0) this.direction = { x: 0, y: 1 };
+          break;
+        case 'ArrowLeft':
+          if (this.direction.x === 0) this.direction = { x: -1, y: 0 };
+          break;
+        case 'ArrowRight':
+          if (this.direction.x === 0) this.direction = { x: 1, y: 0 };
+          break;
+      }
+    },
+    getRandomFoodPosition() {
+      let newFoodPosition;
+      do {
+        newFoodPosition = {
+          x: Math.floor(Math.random() * this.boardSize),
+          y: Math.floor(Math.random() * this.boardSize),
+        };
+      } while (this.snake.some(segment => segment.x === newFoodPosition.x && segment.y === newFoodPosition.y));
+      return newFoodPosition;
+    },
+    drawGame() {
+      this.$nextTick(() => {
+        const gameBoard = this.$refs.gameBoard;
+        gameBoard.innerHTML = ''; // Pulisce il tabellone
 
-},
+        for (let i = 0; i < this.boardSize; i++) {
+          for (let j = 0; j < this.boardSize; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('grid-cell');
 
-changeDirection(event) {
-  // Logica per cambiare direzione
-},
-drawGame() {
-  // Logica per disegnare il gioco
-},
+            if (this.snake.some(segment => segment.x === j && segment.y === i)) {
+              cell.classList.add('snake');
+            } else if (this.food.x === j && this.food.y === i) {
+              cell.classList.add('food');
+            }
+
+            gameBoard.appendChild(cell);
+          }
+        }
+      });
+    },
   },
-mounted() {
-  this.drawGame();
-}
+      mounted() {
+    this.$nextTick(() => {
+      this.drawGame();
+    });
+  },
 }
 </script>
 
